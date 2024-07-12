41e0bdb41740307acdbf582f78a528e1090ce223
Gunnar Siréus <gunnar.sireus@gmail.com>
2024-07-12 19:26:21 +0200

    Add project files.

---
+++ CronJobManager/Program.cs
@@ -0,0 +1,88 @@
+using CronJobManager.Services.Interfaces;
+using CronJobManager.Services.Models;
+using CronJobManager.Services.Services;
+using CronJobManager.Services.Settings;
+using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+using SireusLicenseVerification;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+
+namespace CronJobManager
+{
+    public static class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var licenseManager = new LicenseManager();
+            if (!licenseManager.ValidateLicense("appsettings.json"))
+            {
+                Console.WriteLine("Invalid license key.");
+                Environment.Exit(0);
+            }
+
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    IConfiguration configuration = hostContext.Configuration;
+                    services.AddSingleton(configuration.GetSection("Resources").Get<ServiceSettings>());
+                    services.AddSingleton(configuration.GetSection("Jobs").Get<JobSettings>());
+                    services.AddSingleton<IMemoryCache>(new MemoryCache(new MemoryCacheOptions { SizeLimit = 1024 }));
+                    services.AddLogging(builder =>
+                    {
+                        builder.AddConsole();
+                        // Add other logging providers as necessary
+                    });
+
+                    var connectionString = configuration.GetConnectionString("CronJobManager");
+                    services.AddHangfire(c => c.UseSqlServerStorage(connectionString));
+                    services.AddHangfireServer();
+                    services.AddSingleton<IRestClientFactory, RestClientFactory>();
+                    services.AddTransient<IJobService, JobService>();
+                    services.AddTransient<IJobManager, JobManager>();
+                    services.AddHostedService<CronJobManagerService>();
+
+                    services.AddControllers();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.Configure(app =>
+                    {
+                        var defaultDateCulture = "en-US";
+                        var ci = new CultureInfo(defaultDateCulture);
+
+                        app.UseRequestLocalization(new RequestLocalizationOptions
+                        {
+                            DefaultRequestCulture = new RequestCulture(ci),
+                            SupportedCultures = [ci],
+                            SupportedUICultures = [ci]
+                        });
+
+                        app.UseRouting();
+                        app.UseHangfireDashboard(); // Add this line to use the Hangfire Dashboard on localhost:5000/hangfire
+
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapHangfireDashboard();
+                            endpoints.MapControllers();
+                        });
+                    });
+                });
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Database CronJobaManager must exist in Sql Server before project is started
+Project QueueAPI must be up and running before CronJobManager is started
